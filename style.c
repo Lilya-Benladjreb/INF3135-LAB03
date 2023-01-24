b280412227078a0c58e52d19606cc12bafb86529
Lilya Benladjreb <lilyacbenladjreb@gmail.com>
2023-01-24 14:19:53 -0500

    ajout commentaires

--- style.c
+++ style.c
@@ -1,3 +1,9 @@
+/**
+ * style.c
+ * 
+ * A module that manipulates matrices, for dimensions 3 by 3, by multiplying them and prints them
+ * @author Lilya Benladjreb
+*/
 #include <stdio.h>
 
 #define DIMENSION 3
@@ -5,6 +11,13 @@
 typedef struct {
     double values[DIMENSION][DIMENSION];
 }matrix3x3;
+
+/**
+ * Multiplies two matrix of 3x3
+ * @param m1 matrix
+ * @param m2 matrix
+ * @param m3 matrix
+*/
 
 void multiplyMatrices(matrix3x3 *m1, matrix3x3 *m2, matrix3x3 *m) {
     for (unsigned int i = 0 ; i < DIMENSION ; i++ ) {
@@ -17,11 +30,20 @@ void multiplyMatrices(matrix3x3 *m1, matrix3x3 *m2, matrix3x3 *m) {
     }
 }
 
+/**
+ * Prints a matrix 3x3 with stdout
+ * @param m 
+*/
+
 void printMatrix(const matrix3x3 *m) {
 	printf("[ %lf %lf %lf ]\n", m -> values[0][0], m -> values[0][1], m -> values[0][2]);
 	printf("[ %lf %lf %lf ]\n", m -> values[1][0], m -> values[1][1], m -> values[1][2]);
     printf("[ %lf %lf %lf ]\n", m -> values[2][0], m -> values[2][1], m -> values[2][2]);
 }
+
+/**
+ * Main 
+*/
 
 int main(void) {
     matrix3x3 m1 = {.values = { 1.0, 2.0, 0.0, -1.0, 0.0, 1.0, 0.0, 2.0, 1.0}};
